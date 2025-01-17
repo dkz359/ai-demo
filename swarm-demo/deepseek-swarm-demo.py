8dba184c73c3533026b31400cda0666069d6eafd
Arvin Du <arvin.du@tdsynnex.com>
2025-01-17 09:52:03 +0800

    :sparkles: add swarm demo

---
+++ swarm-demo/deepseek-swarm-demo.py
@@ -0,0 +1,31 @@
+from swarm import Swarm, Agent
+from openai import OpenAI
+import os
+
+client = OpenAI(
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    base_url="https://api.deepseek.com"
+)
+swarm_client = Swarm(client)
+
+def transfer_to_agent_b():
+    return agent_b
+
+
+agent_a = Agent(
+    name="Agent A",
+    instructions="You are a helpful agent.",
+    functions=[transfer_to_agent_b],
+)
+
+agent_b = Agent(
+    name="Agent B",
+    instructions="Only speak in Haikus.",
+)
+
+response = swarm_client.run(
+    agent=agent_a,
+    messages=[{"role": "user", "content": "I want to talk to agent B."}],
+)
+
+print(response.messages[-1]["content"])
